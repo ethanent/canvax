7cfbad20b26cc6a49f33d7fb38790572d53b550c
Ethan Davis <futuristiccake@gmail.com>
2017-07-30 23:04:24 -0700

    Quite a few updates

--- canvax.js
+++ canvax.js
@@ -18,6 +18,7 @@ NOTWITHSTANDING ANYTHING IN CONTRADICTION, THE AUTHORS AND COPYRIGHT HOLDERS OF 
 * @namespace
 */
 const canvax = {
+	"_imageCache": {},
 	/**
 	* Tests if an object is an HTMLElement
 	* @private
@@ -32,6 +33,9 @@ const canvax = {
 		catch (err) {
 			return (typeof obj === "object" && obj.hasOwnProperty("nodeType"));
 		}
+	},
+	"_pointDistance": (p1, p2) => { // [x, y] format
+		return Math.sqrt(Math.pow(Math.abs(p1[0] - p2[0]), 2) + Math.pow(Math.abs(p1[1] - p2[1]), 2));
 	},
 	/**
 	* Renders an entity to a renderer's canvas
@@ -70,8 +74,49 @@ const canvax = {
 					ctx.stroke();
 				}
 				break;
+			case "Ellipse":
+				var kappa = .5522847493;
+				var ox = (entity.width / 2) * kappa;
+				var oy = (entity.height / 2) * kappa;
+				var xe = entity.x + entity.width;
+				var ye = entity.y + entity.height;
+				var xm = entity.x + entity.width / 2;
+				var ym = entity.y + entity.height / 2;
+
+				ctx.beginPath();
+				ctx.moveTo(entity.x, ym);
+				ctx.bezierCurveTo(entity.x, ym - oy, xm - ox, entity.y, xm, entity.y);
+				ctx.bezierCurveTo(xm + ox, entity.y, xe, ym - oy, xe, ym);
+				ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
+				ctx.bezierCurveTo(xm - ox, ye, entity.x, ym + oy, entity.x, ym);
+				ctx.closePath();
+
+				if (entity.backgroundColor !== "none") {
+					ctx.fillStyle = entity.backgroundColor;
+					ctx.fill();
+				}
+				if (entity.borderWeight !== "0" && entity.borderColor !== "none") {
+					ctx.strokeStyle = entity.borderColor;
+					ctx.lineWidth = entity.borderWeight;
+					ctx.stroke();
+				}
+				break;
+			case "Image":
+				if (canvax._imageCache.hasOwnProperty(entity.source)) {
+					var imageSource = canvax._imageCache[entity.source];
+				}
+				else {
+					var imageSource = new Image();
+					imageSource.src = entity.source;
+
+					canvax._imageCache[entity.source] = imageSource;
+				}
+				ctx.drawImage(imageSource, entity.x, entity.y, entity.width, entity.height);
+				break;
 			case "CustomEntity":
 				entity.entityMethod(ctx);
+				break;
+			default:
 				break;
 		}
 	},
@@ -90,6 +135,8 @@ const canvax = {
 	"_clearCanvas": (renderer, element) => {
 		renderer.ctx.clearRect(0, 0, element.width, element.height);
 	},
+	// TODO add collision detection method
+	// Renderer class
 	/**
 	* Returns a renderer object for a canvas element.
 	* @public
@@ -145,7 +192,7 @@ const canvax = {
 	* @property {function} setSize - (width, height)
 	*/
 	"Rectangle": function(x, y, width, height, backgroundColor, borderColor, borderWeight) {
-		if (x && y && width && height) {
+		if (typeof x !== "undefined" && typeof y !== "undefined" && typeof width !== "undefined" && typeof height !== "undefined") {
 			this.type = "Rectangle";
 
 			this.x = x;
@@ -164,9 +211,18 @@ const canvax = {
 				this.y = yset;
 			};
 
-			this.setSize = (width, height) => {
-				this.width = width;
-				this.height = height;
+			this.setSize = (widthSet, heightSet) => {
+				this.width = widthSet;
+				this.height = heightSet;
+			};
+
+			this.getBounds = () => {
+				return {
+					"l": this.x - this.width / 2,
+					"t": this.y + this.height / 2,
+					"r": this.x + this.width / 2,
+					"b": this.y - this.height / 2,
+				};
 			};
 		}
 		else {
@@ -192,7 +248,7 @@ const canvax = {
 	* @property {function} setPosition - (x, y)
 	*/
 	"Circle": function(x, y, radius, backgroundColor, borderColor, borderWeight) {
-		if (x && y && radius) {
+		if (typeof x !== "undefined" && typeof y !== "undefined" && typeof radius !== "undefined") {
 			this.type = "Circle";
 
 			this.x = x;
@@ -209,9 +265,130 @@ const canvax = {
 				this.x = xset;
 				this.y = yset;
 			};
+
+			this.getBounds = () => {
+				var halfRadius = this.radius / 2;
+				return {
+					"l": this.x - halfRadius,
+					"t": this.y + halfRadius,
+					"r": this.x + halfRadius,
+					"b": this.y - halfRadius,
+				};
+			};
 		}
 		else {
 			throw new Error("Missing position parameter.");
+		}
+	},
+	/**
+	* Returns an Ellipse entity.
+	* @public
+	* @class
+	* @param {number} x
+	* @param {number} y
+	* @param {number} width
+	* @param {number} height
+	* @param {string} [backgroundColor=none] - Background color of entity ('none' to render no background)
+	* @param {string} [borderColor=#000000]  - Border color of entity ('none' to render no border)
+	* @param {number} [borderWeight=3] - Border weight of entity in space units ('0' to render no border)
+	* @property {number} x
+	* @property {number} y
+	* @property {number} width
+	* @property {number} height
+	* @property {string} backgroundColor
+	* @property {string} borderColor
+	* @property {string} borderWeight
+	* @property {function} setPosition - (x, y)
+	* @property {function} setSize - (width, height)
+	*/
+	"Ellipse": function(x, y, width, height, backgroundColor, borderColor, borderWeight) {
+		if (typeof x !== "undefined" && typeof y !== "undefined" && typeof width !== "undefined" && typeof height !== "undefined") {
+			this.type = "Ellipse";
+
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+
+			this.backgroundColor = (backgroundColor || "none");
+			this.borderColor = (borderColor || "#000000");
+			this.borderWeight = (borderWeight || "0px");
+
+			// Update methods
+
+			this.setPosition = (xset, yset) => {
+				this.x = xset;
+				this.y = yset;
+			};
+
+			this.setSize = (widthSet, heightSet) => {
+				this.width = widthSet;
+				this.height = heightSet;
+			};
+
+			this.getBounds = () => {
+				return {
+					"l": this.x - this.width / 2,
+					"t": this.y + this.height / 2,
+					"r": this.x + this.width / 2,
+					"b": this.y - this.height / 2,
+				};
+			};
+		}
+		else {
+			throw new Error("Missing position parameter.");
+		}
+	},
+	/**
+	* Returns an Image entity.
+	* @public
+	* @class
+	* @param {number} x - X coordinate of top left corner of image location
+	* @param {number} y - Y coordinate of top left corner of image location
+	* @param {number} width
+	* @param {number} height
+	* @param {string} source
+	* @property {number} x - X coordinate of top left corner of image location
+	* @property {number} y - Y coordinate of top left corner of image location
+	* @property {number} width
+	* @property {number} height
+	* @property {string} source - Image source
+	* @property {function} setPosition - (x, y)
+	* @property {function} setSize - (width, height)
+	*/
+	"Image": function(x, y, width, height, source) {
+		if (typeof x !== "undefined" && typeof y !== "undefined" && typeof width !== "undefined" && typeof height !== "undefined" && source) {
+			this.type = "Image";
+
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			this.source = source;
+
+			// Update methods
+
+			this.setPosition = (xset, yset) => {
+				this.x = xset;
+				this.y = yset;
+			};
+
+			this.setSize = (widthSet, heightSet) => {
+				this.width = widthSet;
+				this.height = heightSet;
+			};
+
+			this.getBounds = () => {
+				return {
+					"l": this.x,
+					"t": this.y,
+					"r": this.x + this.width,
+					"b": this.y - this.height,
+				};
+			};
+		}
+		else {
+			throw new Error("Missing required Image configuration parameter.");
 		}
 	},
 	/**
--- test.js
+++ test.js
@@ -1,32 +1,8 @@
 var game = new canvax.Renderer(document.getElementById("demoCanvas"));
 
-var myRect = new canvax.Rectangle(10, 10, 50, 50, "#3498DB", "none", "0px");
-
-var myEnt = new canvax.Entity((ctx) => {
-	var x = 100;
-	var y = 100;
-	var w = 40;
-	var h = 80;
-	var kappa = .5522848,
-		ox = (w / 2) * kappa, // control point offset horizontal
-		oy = (h / 2) * kappa, // control point offset vertical
-		xe = x + w,           // x-end
-		ye = y + h,           // y-end
-		xm = x + w / 2,       // x-middle
-		ym = y + h / 2;       // y-middle
-
-	ctx.beginPath();
-	ctx.moveTo(x, ym);
-	ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
-	ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
-	ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-	ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
-	ctx.stroke();
-});
+var myRect = new canvax.Image(100, 100, 50, 100, "http://charactersheets.minotaur.cc/images/single.png");
 
 game.add(myRect);
-game.add(myEnt);
-
 
 
 
@@ -46,28 +22,30 @@ animate();
 var keysDown = [];
 
 document.body.addEventListener("keydown", (e) => {
-	if (keysDown.indexOf(e.key) === -1) {
-		keysDown.push(e.key);
+	if (keysDown.indexOf(e.key.toLowerCase()) === -1) {
+		keysDown.push(e.key.toLowerCase());
 	}
 });
 
 document.body.addEventListener("keyup", (e) => {
-	if (keysDown.indexOf(e.key) !== -1) {
-		keysDown.splice(keysDown.indexOf(e.key), 1);
+	if (keysDown.indexOf(e.key.toLowerCase()) !== -1) {
+		keysDown.splice(keysDown.indexOf(e.key.toLowerCase()), 1);
 	}
 });
 
 setInterval(() => {
-	if (keysDown.indexOf("ArrowUp") !== -1) {
+	var a = (canvax._pointDistance([myRect.x, myRect.y], [game.element.width / 2, game.element.height / 2]) > 330);
+	if (a) console.log("Too far from center! Turn back now!");
+	if (keysDown.indexOf("w") !== -1) {
 		myRect.y += -0.6;
 	}
-	if (keysDown.indexOf("ArrowDown") !== -1) {
+	if (keysDown.indexOf("s") !== -1) {
 		myRect.y += 0.6;
 	}
-	if (keysDown.indexOf("ArrowRight") !== -1) {
+	if (keysDown.indexOf("d") !== -1) {
 		myRect.x += 0.6;
 	}
-	if (keysDown.indexOf("ArrowLeft") !== -1) {
+	if (keysDown.indexOf("a") !== -1) {
 		myRect.x += -0.6;
 	}
 }, 1);
